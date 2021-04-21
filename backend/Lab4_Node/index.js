9142079dc253f1f8f73cb9eae99d2059de04c58f
John G. Nguyen <john.nguyen.career@gmail.com>
2021-04-21 17:50:40 -0500

    BACKEND - Node - comments on destructuring

--- backend/Lab4_Node/index.js
+++ backend/Lab4_Node/index.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const app = express();
-const zoo = require('./data.js');
+
+// comment this out because zoo destructure {zoo} needs to happen instead
+// const zoo = require('./data.js');
 
 // require() parameter is an object
 // difference between const {zoo} and const zoo
+// but what is it? Lots of confusion here, deleted some guesses, but
+// data.js = {zoo: array_of_objects}
+// {zoo} = data.js should be a destructuring statement to return array of objects
+// If we don't destructure, and then just print out {zoo}, that is an object assignment with some weird zoo to zoo mapping
+// So, definitely do this
+const {zoo} = require("./data.js");
 
+console.log("The value from the destructured key 'zoo'");
 console.log(zoo);
 
+console.log("The destructured key 'zoo' back in object")
+console.log({zoo});
+
 // Require the data from the data.js file
+	// see above
 
 // Create a root route handler that sends the data from above to browser
 app.get('/', (req, res) => {
   res.send(`${zoo}`);
 });
+
 // 1. Test with Postman
 // 2. Test with CURL
 // 3. Test with the browser
 
 // Build a route handler for /names that sends back h1-encased names of all the animals
-app.get('/names', (req, res) => {
-	let result = zoo.map(el => {
-		`<h1>${el.name}</h1>`
-	}); // end result
-});
 
 // Results should be:
                 //  "Name: <name of the animal>"
+app.get('/names', (req, res) => {
+	let result = zoo.map( el => (
+		 `<h1>${el.name}</h1>`
+	)); // end result, super shorthand, I don't like it QQ 
+  	res.send(result.join(""));
+}); // end get
 
 // Build a route handler for /types that sends back h1-encased types of all the animals
 // Encase the name of the animal in an h2 tag
