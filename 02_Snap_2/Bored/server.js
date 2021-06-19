4b1e328cfd3715f10b59b02634055b40a9bb05e4
John G. Nguyen <john.nguyen.career@gmail.com>
2021-06-19 09:51:06 -0500

    02_SNAP_2 -- 3/4 check in

--- 02_Snap_2/Bored/server.js
+++ 02_Snap_2/Bored/server.js
@@ -9,17 +9,25 @@ app.get('/', (req, res) => {
 //	res.send("I am groute!!!");
 })
 
-app.get('/getActivity', (req, res) => {
-    const { type, price, participants } = req.params
+// app.get('/getActivity', (req, res) => {
+app.get('/activity', (req, res) => {
+    const { type, price, participants } = req.params;
 
-    const urlStr = `http://www.boredapi.com/api/activity?type=${type}&minprice=0&maxprice=${price}&participants=${participants}`
+    // error is in here with how the params are being fed in
+    const urlStr = `http://www.boredapi.com/api/activity?type=${type}&minprice=0&maxprice=${price}&participants=${participants}`;
 
+// This hardcoded endpoint doesn't give an error, so the error is with the variables being fed to the above endpoint
+//    const urlStr = "http://www.boredapi.com/api/activity?type=education&minprice=0&maxprice=1&participants=1";
+
+//    const urlStr = 'https://dog.ceo/api/breeds/image/random';
     fetch(urlStr)
         .then(res => res.json())
         .then(data => {
-            if(data.error) res.render('error.ejs', { error: data.error })
-
-            else res.render('result.ejs', { data })
+            if(data.error) {
+                res.render('error.ejs', { error: data.error } )
+            } else  {
+            res.render('result.ejs', { data });
+            }
         })
         .catch(err => {
             console.log('There was an error: ', err)
