19b8fb705d8f3fa06d7c5db803c72a4d5082ac24
John G. Nguyen <john.nguyen.career@gmail.com>
2021-06-19 09:31:17 -0500

    02_SNAP_2 -- midway commit

---
+++ 02_Snap_2/Bored/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+// const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    let types = ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+    res.render('home.ejs', { type: types })
+//	res.send("I am groute!!!");
+})
+
+app.get('/getActivity', (req, res) => {
+    const { type, price, participants } = req.params
+
+    const urlStr = `http://www.boredapi.com/api/activity?type=${type}&minprice=0&maxprice=${price}&participants=${participants}`
+
+    fetch(urlStr)
+        .then(res => res.json())
+        .then(data => {
+            if(data.error) res.render('error.ejs', { error: data.error })
+
+            else res.render('result.ejs', { data })
+        })
+        .catch(err => {
+            console.log('There was an error: ', err)
+        })
+})
+
+const port = 8080 || process.env.PORT;
+// app.listen(port, () => {
+//    console.log('App listening on port 3000')
+// })
+app.listen(port, () => console.log(`Listening on port ${port}`));
