55be8cb19ee7eab74f4f360f32eef85837ec146a
John G. Nguyen <john.nguyen.career@gmail.com>
2021-07-24 22:43:56 -0500

    FINAL - practical -- cleaned up, added trending API x1

--- 03_FINAL/D_PRACTICAL/07_Lyrics_Back_Starter/index.js
+++ 03_FINAL/D_PRACTICAL/07_Lyrics_Back_Starter/index.js
@@ -10,27 +10,47 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
+// so request needs to be installed
+const request = require('request');
+
 // API
-//  https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime
-// const baseUrl = 'https://api.lyrics.ovh/v1';
-const baseUrl = 'https://api.giphy.com/v1/gifs/search';
-const api_key = "73H9V6lKlhDik4GLmoJG2CeTj9eTRuUS";
 
 // giphy endpoint
 // api.giphy.com/v1/gifs/search
 // request parameters - these are query parameters, not params parameters
 // api_key = 
 // &q search string parameter
-
-// so request needs to be installed
-const request = require('request');
+const baseUrl = 'https://api.giphy.com/v1/gifs/search';
+const api_key = "73H9V6lKlhDik4GLmoJG2CeTj9eTRuUS";
 
 // ROUTES
-app.get('/', (req, res)=>{
-  res.render('home');
+// Home - Trending API
+app.get( '/', ( req, res ) => {
+	// borrow API format from Search API below
+	let endpoint = 'https://api.giphy.com/v1/gifs/trending?api_key=73H9V6lKlhDik4GLmoJG2CeTj9eTRuUS&limit=1';
+
+	request( endpoint, ( error, response, body ) => {
+	    if( error || response.statusCode != 200 ) {
+			res.render( 'error', { error: "ERROR" });
+		} else {
+			// borrow from Search API below
+		    let gifobj = JSON.parse( body ); // use with request and needle
+			let { data } = gifobj; // would return an array of objects, data for the gif
+			let { images } = data[0];
+			let image = images.downsized.url;
+			res.render( 'home', { image });
+	    }
+	}) // end request
+
+}) // end Home
+
+// Search
+app.get('/search', (req, res)=>{
+  res.render('search');
 })
 
-app.get('/getGiphy', ( req, res ) => {
+// Search API
+app.get('/searchGiphy', ( req, res ) => {
 	// destructure q key from query object
 	let { q } = req.query;
 
@@ -47,7 +67,7 @@ app.get('/getGiphy', ( req, res ) => {
 	*/
 
 	// let endpoint = `${ baseUrl }/${ q }/&api_key=${ api_key }`;
-	// hardcode debug
+	// Can't get to work, so hard coding for debug
 	let endpoint = 'https://api.giphy.com/v1/gifs/search?q=chonk&api_key=73H9V6lKlhDik4GLmoJG2CeTj9eTRuUS&limit=1';
 	
 	request(endpoint, (error, response, body) => {
@@ -85,66 +105,6 @@ app.get('/getGiphy', ( req, res ) => {
       res.render('error', {error: "ERROR"});
     }
   })
-})
-
-// This route is not gone to directly
-// This route is called from home
-// This is why the variables "artist" and "title" appear in the query, because they are called from the home page
-// which is also calling getLyrics route
-app.get('/getLyrics', (req, res)=>{
-  console.log(req);
-  // output is
-  /*
-  params: {},
-  query: { artist: '1', title: '3' },
-  res: <ref *3> ServerResponse {
-    _events: [Object: null prototype] {
-      finish: [Array],
-      end: [Function: onevent]
-    },
-  */
-  // It's query (versus params) based on input method, in this case the form submit buttons
-  console.log(req.query.title);
-  res.render('example');
-})
-
-app.get('/getMoreLyrics', (req, res) => {
-  // destructure
-  let {title, artist} = req.query;
-
-  // this is the full API URL
-  let endpoint = `${baseUrl}/${artist}/${title}`;
-
-  // request, instead of fetch or needle, needs to be installed - makes http request
-
-  // steps for http request
-  // making a request using endpoint
-  // receive a response - generally json
-    // parse it!!!
-  // do something
-  // error handling
-
-  // The way request works -
-  // positional arguments
-  request(endpoint, (error, response, body) => {
-    // previously assumed no errors, now will handle error
-    if(!error && response.statusCode == 200) {
-	    // convert to JSON
-	    // return response.json()
-	    let parsedData = JSON.parse(body); // use with request and needle
-	    console.log(parsedData); // make sure it's okay
-	    // parsedData.lyrics - object dot notation - will give lyrics
-	    // then use regex to remove new lines
-	    // let data = parsedData.lyrics;
-	    let data = parsedData.lyrics.replace(/\n/g, "<br>");
-	    // res.render('results', {data});
-	    res.render('results', {data, artist, title});
-    } else {
-      res.render('error', {error: "No lyrics found"});
-    }
-  })
-
-//  res.render('results');
 })
 
 // LISTENER
